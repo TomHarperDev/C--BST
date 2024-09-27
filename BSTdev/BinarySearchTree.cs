66fc413f4b009c155993408ba0ab69c80a34a6c6
TomHarperDev <18harper.t@lifesciencesutc.net>
2024-09-27 09:47:42 +0100

    Add project files.

---
+++ BSTdev/BinarySearchTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTdev
+{
+    internal class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree(string name)
+        {
+            
+
+
+        }
+    }
+
+    public class Node
+    {
+        public int ID;
+        public string Name;
+    }
+}
---
+++ BSTdev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSTdev
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+        }
+
+}
