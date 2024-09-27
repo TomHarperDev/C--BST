cc412cdb15d98d1b04999099479c655146059a19
TomHarperDev <18harper.t@lifesciencesutc.net>
2024-09-27 10:47:21 +0100

    commit

--- BSTdev/BinarySearchTree.cs
+++ BSTdev/BinarySearchTree.cs
@@ -12,15 +12,49 @@ namespace BSTdev
 
         public BinarySearchTree(string name)
         {
-            
+            this.root = new Node(10, name);
+        }
+
+        public void AddValueToTree(BinarySearchTree currentTree, Node CurrentNode, int value, string name)
+        {
+            if (currentTree.root == null) 
+            {
+                Console.WriteLine("Tree is null");
+                return;
+            }
+            if (CurrentNode == null)
+            {
+                CurrentNode = new Node(value, name);
+                return;
+            }
 
 
+            if (CurrentNode.value < value) 
+            {
+                AddValueToTree(currentTree, CurrentNode.rightNode, value, name);
+            }
+            if (CurrentNode.value > value)
+            {
+                AddValueToTree(currentTree, CurrentNode.leftNode, value, name);
+            }
         }
     }
 
     public class Node
     {
-        public int ID;
+        
+        public int value;
         public string Name;
+
+        public Node leftNode;
+        public Node rightNode;
+
+
+
+        public Node(int value, string name)
+        {
+            this.value = value;
+            this.Name = name;
+        }
     }
 }
--- BSTdev/Program.cs
+++ BSTdev/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BSTdev
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            
-        }
+            BinarySearchTree testTree = new BinarySearchTree("root");
 
+
+            testTree.AddValueToTree(testTree, testTree.root, 5, "left child");
+            testTree.AddValueToTree(testTree, testTree.root, 14, "right child");
+
+            Console.WriteLine(testTree.root.Name);
+            Console.WriteLine(testTree.root.rightNode.Name);
+            Console.WriteLine(testTree.root.leftNode.Name);
+        }
+    }
 }
